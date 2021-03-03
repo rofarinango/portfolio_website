97a6a0549065edec3f72b9236f5fb709c85a7a14
rofarinango <rodrigofarii@gmail.com>
2021-03-03 16:52:46 -0500

    feat: Route page creation

--- pages/urls.py
+++ pages/urls.py
@@ -7,6 +7,15 @@ app_name = 'pages'
 urlpatterns = [
     #Home page
     path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     #Page that show about section.
     path('about/', views.about, name='about'),
+    # Page that show services section.
+    path('services/', views.services, name='services'),
+    #Page that show skills section.
+    path('skills/', views.skills, name='skills'),
+    #Page that show teams section.
+    path('teams/', views.teams, name='teams'),
+    #Page that show contact section.
+    path('contact/', views.contact, name='contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,24 @@ from django.shortcuts import render
 
 def home(request):
     """The home page for personal website."""
-    return render(request, 'pages/base.html')
+    return render(request, 'pages/home.html')
 
 def about(request):
     """The about page for personal website."""
     return render(request, 'pages/about.html')
+
+def services(request):
+    """The services page for personal website."""
+    return render(request, 'pages/services.html')
+
+def skills(request):
+    """The skills page for personal website."""
+    return render(request, 'pages/skills.html')
+
+def teams(request):
+    """The teams page for personal website."""
+    return render(request, 'pages/teams.html')
+
+def contact(request):
+    """Tje contact page for personal website."""
+    return render(request, 'pages/contact.html')
