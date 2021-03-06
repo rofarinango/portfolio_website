3c09507a27865bd3ec983eccf0b663373a8f9cc4
Rodrigo Farinango <rodrigofarii@gmail.com>
2021-03-05 20:49:25 -0500

    read titles of projects from github api rest

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,21 @@ from django.views.generic import View
 from django.shortcuts import render
 from github import Github
 
-from projects.models import Project
+class Project:
+    "A project class from our repository on Github."
+    title = ""
+    description = ""
+    language = ""
+    date_created = ""
+    size = 0
+    stars = 0
+    def __init__(self, title, description, language, date_created, size, stars):
+        self.title = title
+        self.description = description
+        self.language = language
+        self.date_created = date_created
+        self.size = size
+        self. stars = stars
 
 def home(request):
     """The home page for personal website."""
@@ -18,9 +32,12 @@ def skills(request):
 def projects(request):
     """The projects page for personal website."""
     g = Github("CHANGE_token")
+    projects = []
     for repo in g.get_user().get_repos():
-        print(repo.description)
-    return render(request, 'pages/projects.html')
+        project = Project(repo.name, repo.description, repo.language, repo.created_at, repo.size, repo.stargazers_count)
+        projects.append(project)
+    context = {'projects': projects}
+    return render(request, 'pages/projects.html', context)
 
 def contact(request):
     """Tje contact page for personal website."""
